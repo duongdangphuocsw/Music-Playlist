dd7d02fe3710c9d7120ec9484f78109d8cb98b11
dph_sw <duongdangphuoc.bk@gmail.com>
2022-10-04 00:03:56 +0700

    up file

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+    1. Render song
+    2. scroll top
+    3.Play/ pause/ seek
+    4. CD rotate
+    5. Next/ prev
+    6. Random
+    7. Next/ Repeat when ended
+    8. Active song
+    9. Scroll active song into view
+    10. Play song when click
+*/
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
